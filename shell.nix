c1706f5a00a63f01d1b6234b2d014f32f4a2e2ff
Arnav Gawas <94751172+aln730@users.noreply.github.com>
2026-07-14 13:27:00 -0400

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ system ? builtins.currentSystem }:
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    node = lock.nodes.flake-compat.locked;
+  in
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${node.rev}.tar.gz";
+    sha256 = node.narHash;
+  }
+) { src = ./.; }).shellNix
