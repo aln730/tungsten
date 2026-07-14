51d2b2a1c2c872d2864affa52050f5deb10ed2c5
Arnav Gawas <94751172+aln730@users.noreply.github.com>
2026-07-14 13:31:25 -0400

    home/default.nix

---
+++ home/default.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  imports = [
+    ../modules/home-manager/shell.nix
+    ../modules/home-manager/git.nix
+    ../modules/home-manager/editors.nix
+    ../modules/home-manager/programming.nix
+    ../modules/home-manager/desktop.nix
+  ];
+
+  home.username = "zxcv";
+  home.homeDirectory = "/home/zxcv";
+
+  # Pins the baseline behavior of Home-Manager-managed programs.
+  # Do not bump this on upgrades; it exists for backwards-compat, not
+  # version tracking. See the Home Manager release notes before ever
+  # changing it.
+  home.stateVersion = "26.05";
+
+  # Lets Home Manager manage itself (adds the `home-manager` CLI).
+  programs.home-manager.enable = true;
+}
