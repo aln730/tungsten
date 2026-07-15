656e808233e026fadccfc83211b802185ed34f38
Arnav Gawas <arnsg730@proton.me>
2026-07-15 17:26:48 -0400

    stuff

--- modules/home-manager/deps.nix
+++ modules/home-manager/deps.nix
@@ -15,5 +15,7 @@
     LIBFREEFARE_PATH = "${pkgs.libfreefare}/lib";
     LIBNFC_PATH = "${pkgs.libnfc}/lib";
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.linux-pam}/include";
+    RUSTFLAGS = "-L ${pkgs.linux-pam}/lib";
   };
 }
