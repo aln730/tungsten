c761e46e3aa41cacdd1271818cbfe9e9709b536a
Arnav Gawas <arnsg730@proton.me>
2026-07-17 00:20:04 -0400

    gnome stuff

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -5,5 +5,19 @@
     spotify
     alacritty
     firefox
+    gnomeExtensions.vitals
   ];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "Vitals@CoreCoding.com"
+      ];
+    };
+
+    "org/gnome/shell/extensions/vitals" = {
+      hot-sensors = [ "_processor_usage_" "_memory_usage_" "_temperature_" "_network-rx_" ];
+      position-in-panel = 1;
+    };
+  };
 }
