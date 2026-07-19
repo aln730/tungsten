f4589d3f43df204b63a4f2c695f55efc18a4d612
Arnav Gawas <arnsg730@proton.me>
2026-07-19 16:02:33 -0400

    elcord configs

--- home/doom/config.el
+++ home/doom/config.el
@@ -21,9 +21,6 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -73,6 +70,11 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(use-package! elcord)
-:config
-(elcord-mode)
+(use-package! elcord
+  :config
+  (setq elcord-buffer-details-format-function
+        (lambda ()
+          (if-let ((proj (project-current)))
+              (format "Editing %s <%s>" (buffer-name) (file-name-nondirectory (directory-file-name (project-root proj))))
+            (format "Editing %s" (buffer-name)))))
+  (elcord-mode))
